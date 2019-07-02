29978ea700e9cdb400f421909fc6d5562e0ec676
mikewiebe <mwiebe@cisco.com>
2019-07-02 16:29:33 -0400

    Scaffolding code

---
+++ lib/ansible/module_utils/network/nxos/config/telemetry/telemetry.py
@@ -0,0 +1,153 @@
+#
+# -*- coding: utf-8 -*-
+# Copyright 2019 Cisco and/or its affiliates.
+# GNU General Public License v3.0+
+# (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+"""
+The nxos_telemetry class
+It is in this file where the current configuration (as dict)
+is compared to the provided configuration (as dict) and the command set
+necessary to bring the current configuration to it's desired end-state is
+created
+"""
+from ansible.module_utils.network.common.cfg.base import ConfigBase
+from ansible.module_utils.network.common.utils import to_list
+from ansible.module_utils.network.nxos.facts.facts import Facts
+
+
+class Telemetry(ConfigBase):
+    """
+    The nxos_telemetry class
+    """
+
+    gather_subset = [
+        '!all',
+        '!min',
+    ]
+
+    gather_network_resources = [
+        'telemetry',
+    ]
+
+    def __init__(self, module):
+        super(Telemetry, self).__init__(module)
+
+    def get_telemetry_facts(self):
+        """ Get the 'facts' (the current configuration)
+
+        :rtype: A dictionary
+        :returns: The current configuration as a dictionary
+        """
+        facts, _warnings = Facts(self._module).get_facts(self.gather_subset, self.gather_network_resources)
+        telemetry_facts = facts['ansible_network_resources'].get('telemetry')
+        if not telemetry_facts:
+            return []
+        return telemetry_facts
+
+    def execute_module(self):
+        """ Execute the module
+
+        :rtype: A dictionary
+        :returns: The result from module execution
+        """
+        result = {'changed': False}
+        commands = list()
+        warnings = list()
+
+        existing_telemetry_facts = self.get_telemetry_facts()
+        commands.extend(self.set_config(existing_telemetry_facts))
+        if commands:
+            if not self._module.check_mode:
+                self._connection.edit_config(commands)
+            result['changed'] = True
+        result['commands'] = commands
+
+        changed_telemetry_facts = self.get_telemetry_facts()
+
+        result['before'] = existing_telemetry_facts
+        if result['changed']:
+            result['after'] = changed_telemetry_facts
+
+        result['warnings'] = warnings
+        return result
+
+    def set_config(self, existing_telemetry_facts):
+        """ Collect the configuration from the args passed to the module,
+            collect the current configuration (as a dict from facts)
+
+        :rtype: A list
+        :returns: the commands necessary to migrate the current configuration
+                  to the desired configuration
+        """
+        want = self._module.params['config']
+        have = existing_telemetry_facts
+        resp = self.set_state(want, have)
+        return to_list(resp)
+
+    def set_state(self, want, have):
+        """ Select the appropriate function based on the state provided
+
+        :param want: the desired configuration as a dictionary
+        :param have: the current configuration as a dictionary
+        :rtype: A list
+        :returns: the commands necessary to migrate the current configuration
+                  to the desired configuration
+        """
+        state = self._module.params['state']
+        if state == 'overridden':
+            kwargs = {}
+            commands = self._state_overridden(**kwargs)
+        elif state == 'deleted':
+            kwargs = {}
+            commands = self._state_deleted(**kwargs)
+        elif state == 'merged':
+            kwargs = {}
+            commands = self._state_merged(**kwargs)
+        elif state == 'replaced':
+            kwargs = {}
+            commands = self._state_replaced(**kwargs)
+        return commands
+
+    @staticmethod
+    def _state_replaced(**kwargs):
+        """ The command generator when state is replaced
+
+        :rtype: A list
+        :returns: the commands necessary to migrate the current configuration
+                  to the desired configuration
+        """
+        commands = []
+        return commands
+
+    @staticmethod
+    def _state_overridden(**kwargs):
+        """ The command generator when state is overridden
+
+        :rtype: A list
+        :returns: the commands necessary to migrate the current configuration
+                  to the desired configuration
+        """
+        commands = []
+        return commands
+
+    @staticmethod
+    def _state_merged(**kwargs):
+        """ The command generator when state is merged
+
+        :rtype: A list
+        :returns: the commands necessary to merge the provided into
+                  the current configuration
+        """
+        commands = []
+        return commands
+
+    @staticmethod
+    def _state_deleted(**kwargs):
+        """ The command generator when state is deleted
+
+        :rtype: A list
+        :returns: the commands necessary to remove the current configuration
+                  of the provided objects
+        """
+        commands = []
+        return commands
